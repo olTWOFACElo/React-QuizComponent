de68c1bd9f954ade3afe7cbf960d770f1b8e82d1
olTWOFACElo <CRivieccio@HRSA.Gov>
2019-11-14 14:25:44 -0500

    module 5

--- src/Quiz.js
+++ src/Quiz.js
@@ -11,6 +11,12 @@ class Quiz extends Component {
     this.state = { quiz_position: 1 };
   }
 
+  showNextQuestion() {
+    this.setState(state => {
+      return { quiz_position: state.quiz_position + 1 };
+    });
+  }
+
   render() {
     const isQuizEnd =
       this.state.quiz_position - 1 == quizData.quiz_questions.length;
@@ -19,6 +25,7 @@ class Quiz extends Component {
     ) : (
       <QuizQuestion
         quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]}
+        showNextQuestionHandler={this.showNextQuestion.bind(this)}
       />
     );
   }
