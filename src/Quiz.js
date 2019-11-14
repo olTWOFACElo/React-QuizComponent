cebdee9ae02cc29ebb27f0b64bc5463da33c3ea4
olTWOFACElo <CRivieccio@HRSA.Gov>
2019-11-14 14:03:10 -0500

    Module 4 changes

--- src/Quiz.js
+++ src/Quiz.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 
 import QuizQuestion from "./QuizQuestion.js";
+import QuizEnd from "./QuizEnd.js";
 
 let quizData = require("./quiz_data.json");
 
@@ -11,7 +12,11 @@ class Quiz extends Component {
   }
 
   render() {
-    return (
+    const isQuizEnd =
+      this.state.quiz_position - 1 == quizData.quiz_questions.length;
+    return isQuizEnd ? (
+      <QuizEnd />
+    ) : (
       <QuizQuestion
         quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]}
       />
